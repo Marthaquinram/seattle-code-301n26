eb4be5b8c10d791bd7143473a761015e88d61479
Hexx King <hexxking13@gmail.com>
2022-05-21 12:21:08 -0700

    adds demo from lecture

--- class-09/demo/jobs/backend/server.js
+++ class-09/demo/jobs/backend/server.js
@@ -3,8 +3,8 @@
 const express = require('express');
 require('dotenv').config();
 const cors = require('cors');
-const getJobs = require('./lib/jobs');
-const notFound = require('./lib/notFound');
+const getJobs = require('./modules/jobs');
+const notFound = require('./modules/notFound');
 
 const app = express();
 
@@ -13,6 +13,8 @@ app.use(cors());
 const PORT = process.env.PORT || 3001;
 
 app.get('/jobs', getJobs);
+// '*' in programming is called a wildcard and it selects "everything"
+// this endpoint says if you arrive at any other endpoint besides one that we have already defined ^^^ then run the notFound function
 app.use('*', notFound);
 
 app.listen(PORT, () => {
